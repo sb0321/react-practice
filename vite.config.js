f8f74a153e6207a23d3563cc16a6c7345ab9458a
sb0321 <woltaryksb1@gmail.com>
2025-05-07 08:02:26 +0000

    자동 업데이트 수정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +8,7 @@ export default defineConfig({
     port: 5173,
     host: '127.0.0.1',
     watch: {
-      usePolling: true
+      usePolling: true,
     },
-  }
-})
-
+  },
+});
